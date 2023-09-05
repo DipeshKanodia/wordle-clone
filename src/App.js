33b88c6b90fda1f5e7ce0bc7b1fd78b52fc6e247
DipeshKanodia <dkanodia195@gmail.com>
2023-09-05 23:48:31 +0530

    Add data file and fetch word

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [word, setWord] = useState(null);
+
+  useEffect(() => {
+    fetch('./words.json')
+      .then(res => res.json())
+      .then(data => {
+        // random int between 0 & 14
+        var id = Math.floor(Math.random()*data.words.length)
+        setWord(data.words[id]);
+      })
+  }, [])
+  
   return (
     <div className="App">
-      <h1>Wordle</h1>
+      {/* <button onClick={readJsonFile}>Read JSON File</button> */}
+      {word && <h1>word is: {word}</h1>}
     </div>
   );
 }
 
 export default App;
+
+
+// https://api.dictionaryapi.dev/api/v2/entries/en/beats
